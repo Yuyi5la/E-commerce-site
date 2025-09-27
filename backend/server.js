e574469d7e50c7d8238cc0d5cf030dd72ea4af48
ella <randomgive98@gmail.com>
2025-09-27 15:28:15 -0700

    cors error

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,37 @@
-import  express from "express";
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js"; 
 import userRoutes from "./routes/userRoutes.js";
-import cartRoutes from "./routes/cartRoutes.js"
-import prisma from "./prismaClient.js"; // centralized prisma
-
+import cartRoutes from "./routes/cartRoutes.js";
+import prisma from "./prismaClient.js";
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
+const corsOptions = {
+  origin: "https://e-commerce-site-smoky-ten.vercel.app", // frontend URL
+  credentials: true,
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"]
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-
-app.use(cors({
-  origin: "https://e-commerce-site-smoky-ten.vercel.app", // frontend URL
-  credentials: true, // allow cookies/auth headers
-}));
-
 // Routes
-app.use ("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes);
 app.use("/api", productRoutes);
 app.use("/api", userRoutes);
 app.use("/api", cartRoutes);
 
-
-// health check route
+// Health check
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
