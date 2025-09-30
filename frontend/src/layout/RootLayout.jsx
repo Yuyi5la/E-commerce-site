130d6baff4f7419e41a187e139d1e3044113af06
ella <randomgive98@gmail.com>
2025-09-30 06:38:58 -0700

    fixed root layout

--- frontend/src/layout/RootLayout.jsx
+++ frontend/src/layout/RootLayout.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import Navbar from '../components/navbar'
+import React from "react";
+import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <div>
-      <Navbar/>
-      <Outlet/>
+    <div className="flex flex-col min-h-screen">
+     
+      <Navbar />
+
+     
+      <main className="flex-1">
+        <Outlet />
+      </main>
+
+      
       <Footer />
-
     </div>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
